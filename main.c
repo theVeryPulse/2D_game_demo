3c98d4fc3d7abda803d5e6ccb3b9f1e8db9dc788
Philip Li <macpilot23@outlook.com>
2024-09-27 19:01:01 +0100

    feat (player): basic move and jump

--- main.c
+++ main.c
@@ -51,7 +51,7 @@
 
      - raylib/examples/<category>/<category>_example_name.c
      - raylib/examples/<category>/<category>_example_name.png
-     - raylib/examples/<category>/resources/*.*
+     - raylib/examples/<category>/resources/ *.*
      - raylib/examples/Makefile
      - raylib/examples/Makefile.Web
      - raylib/examples/README.md
@@ -64,8 +64,25 @@
      - raylib.com/examples/<category>/<category>_example_name.js
 */
 
-
 #include "raylib.h"
+#include <math.h>
+
+#define FPS 60
+#define MAX_FRAME_RATE 15
+#define MIN_FRAME_RATE 1
+
+#define SPRITE_MAX_SPEED 5
+
+#define GROUND 600
+
+typedef struct Player
+{
+    Vector2 position;
+    Vector2 speed;
+    float   acceleration;
+    float   acceleration_air;
+    float   jump_velocity;
+} Player;
 
 int main(void)
 {
@@ -76,13 +93,30 @@ int main(void)
 
     InitWindow(screen_width, screen_height, "2D Game Demo");
 
-    // TODO: Load resources / Initialize variables at this point
+    // Load resources / Initialize variables
 
     // NOTE: Textures MUST be loaded after Window initialization (OpenGL context
     // is required)
-    Texture2D scarfy = LoadTexture("resources/scarfy.png"); // Texture loading
+    Texture2D scarfy          = LoadTexture("resources/scarfy.png");
+    Rectangle frame_rectangle = {0.0f, 0.0f, (float)scarfy.width / 6,
+                                 (float)scarfy.height};
 
-    SetTargetFPS(60);
+    const int sprite_frame_rate    = 8;
+    int       sprite_frame_index   = 0;
+    int       sprite_frame_counter = 0;
+
+    Vector2 sprite_position = {.x = screen_width / 2, .y = GROUND};
+    Vector2 sprite_velocity = {.x = 0.0f, .y = 0.0f};
+
+    const float sprite_acceleration     = 0.7f;
+    const float sprite_acceleration_air = 0.1f;
+    const float gravity_acceleration    = 0.8f;
+    const float jump_velocity           = -20.0f;
+
+    bool      sprite_in_air = false;
+    const int max_jumps     = 2;
+    int       jumps_left    = max_jumps;
+    SetTargetFPS(FPS);
     //--------------------------------------------------------------------------
 
     // Main game loop
@@ -90,32 +124,101 @@ int main(void)
     {
         // Update
         //----------------------------------------------------------------------
-        // TODO: Update variables / Implement example logic at this point
+
+        // Moves left/right
+        if (sprite_position.x < screen_width - frame_rectangle.width
+            && (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)))
+        {
+            if (frame_rectangle.width < 0)
+                frame_rectangle.width = -frame_rectangle.width;
+            if (sprite_velocity.x < SPRITE_MAX_SPEED)
+            {
+                if (sprite_in_air)
+                    sprite_velocity.x += sprite_acceleration_air;
+                else
+                    sprite_velocity.x += sprite_acceleration;
+            }
+            ++sprite_frame_counter;
+        }
+        else if (sprite_position.x > 0
+                 && (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)))
+        {
+            if (frame_rectangle.width > 0)
+                frame_rectangle.width = -frame_rectangle.width;
+            if (-sprite_velocity.x < SPRITE_MAX_SPEED)
+            {
+                if (sprite_in_air)
+                    sprite_velocity.x -= sprite_acceleration_air;
+                else
+                    sprite_velocity.x -= sprite_acceleration;
+            }
+            ++sprite_frame_counter;
+        }
+        else if (fabs(sprite_velocity.x) < sprite_acceleration)
+            sprite_velocity.x = 0;
+        else if (sprite_velocity.x > 0)
+            sprite_velocity.x -= sprite_acceleration;
+        else if (sprite_velocity.x < 0)
+            sprite_velocity.x += sprite_acceleration;
+
+        // Jump
+        if (jumps_left > 0
+            && (IsKeyPressed(KEY_SPACE) || IsKeyPressed(KEY_UP)
+                || IsKeyPressed(KEY_W)))
+        {
+            sprite_velocity.y = jump_velocity;
+            sprite_in_air     = true;
+            --jumps_left;
+        }
+
+        // Falls while in air
+        if (sprite_in_air)
+            sprite_velocity.y += gravity_acceleration;
+        // Stops falling at ground
+        if (sprite_position.y > GROUND)
+        {
+            sprite_in_air     = false;
+            sprite_velocity.y = 0;
+            sprite_position.y = GROUND;
+            jumps_left        = max_jumps;
+        }
+
+        sprite_position.x += sprite_velocity.x;
+        sprite_position.y += sprite_velocity.y;
+
+        if (sprite_frame_counter >= FPS / sprite_frame_rate)
+        {
+            sprite_frame_counter = 0;
+            ++sprite_frame_index;
+            if (sprite_frame_index > 5)
+                sprite_frame_index = 0;
+            frame_rectangle.x = (float)sprite_frame_index * (float)scarfy.width
+                                / 6;
+        }
         //----------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------
         BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        // TODO: Draw everything that requires to be drawn at this point:
-
-        DrawText("Congrats! You created your first window!", 190, 200, 20,
-                 PINK); // Example
-
+        {
+            ClearBackground(RAYWHITE);
+            // Draw everything:
+            DrawTexture(scarfy, 15, 40, WHITE);
+            DrawRectangleLines(15, 40, scarfy.width, scarfy.height, LIME);
+            DrawRectangleLines(
+                15 + (int)frame_rectangle.x, 40 + (int)frame_rectangle.y,
+                (int)frame_rectangle.width, (int)frame_rectangle.height, RED);
+            // Draw target frame of the sprite
+            DrawTextureRec(scarfy, frame_rectangle, sprite_position, WHITE);
+        }
         EndDrawing();
         //----------------------------------------------------------------------
     }
 
     // De-Initialization
     //--------------------------------------------------------------------------
-
-    // TODO: Unload all loaded resources at this point
-
     UnloadTexture(scarfy); // Texture unloading
-
-    CloseWindow(); // Close window and OpenGL context
+    CloseWindow();         // Close window and OpenGL context
     //--------------------------------------------------------------------------
 
     return 0;
