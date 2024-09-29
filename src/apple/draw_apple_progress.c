94201e440633984e8113347e57b2c3e0a6a5f536
Philip Li <macpilot23@outlook.com>
2024-09-29 22:48:59 +0100

    feat (apple): adjust progress bar position and gap

--- src/apple/draw_apple_progress.c
+++ src/apple/draw_apple_progress.c
@@ -9,7 +9,7 @@
 void draw_apple_progress(int apple_count, int collected_apple_count)
 {
     for (int i = 0; i < apple_count; ++i)
-        DrawRectangleLines(i * 50 + 10, 10, 40, 40, GREEN);
+        DrawRectangleLines(i * 60 + 50, 50, 40, 40, GREEN);
     for (int i = 0; i < collected_apple_count; ++i)
-        DrawRectangle(i * 50 + 10, 10, 40, 40, GREEN);
+        DrawRectangle(i * 60 + 50, 50, 40, 40, GREEN);
 }
